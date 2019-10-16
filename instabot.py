3768323f8c58f37cad2eccbed4de5ddf3526c3cf
Xavier Le Cunff <lecunffxavier@gmail.com>
2019-10-16 19:48:21 +0200

    ok

--- instabot.py
+++ instabot.py
@@ -5,7 +5,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 from datetime import datetime
+from termcolor import colored
 import time
 import random
 import json
@@ -16,8 +18,8 @@ insta_user = "lecunffxavier@gmail.com"
 insta_password = "Chatillon92320"
 hashtag_file = "hashtags.txt"
 number_followers = 50
-max_abos = 200
-nb_days_to_unfollow = 7
+max_abos = 100
+nb_days_to_unfollow = 2
 
 ############################################################
 
@@ -40,8 +42,16 @@ followed = json.load(json_file1)
 json_file = open('magic_book.json')
 following = json.load(json_file)
 p = json_parser()
+nb_followed = 0
+nb_unfollowed = 0
 
 # check if there is someone to unfollow
+
+
+def conclusion():
+    print()
+    print("Number of account followed: " + colored(nb_followed, 'red'))
+    print("Number of account unfollowed: " + colored(nb_unfollowed, 'green'))
 
 
 def is_followed(x):
@@ -58,12 +68,15 @@ def follow(url):
     driver.get(url)
     driver.implicitly_wait(5)
     time.sleep(1)
+    scroll_randomly()
     if not is_followed(url):
         driver.find_element_by_xpath("/html/body/span/section/main/div/header/section/div[1]/div[1]/span/span[1]/button").click()
         followed['followed'].append({
             "url": url
         })
         print("user " + url + " followed")
+        global nb_followed
+        nb_followed = nb_followed + 1
     else:
         print("user " + url + " has already been followed, skiping...")
     time.sleep(1)
@@ -75,7 +88,9 @@ def unfollow(url):
     time.sleep(1)
     driver.find_element_by_xpath("/html/body/span/section/main/div/header/section/div[1]/div[1]/span/span[1]/button").click()
     driver.find_element_by_xpath("/html/body/div[3]/div/div/div[3]/button[1]").click()
-    print("user " + url + " unfollowed")
+    print("user " + url + " unfollowed, bye bye!")
+    global nb_unfollowed
+    nb_unfollowed = nb_unfollowed + 1
     time.sleep(1)
 
 
@@ -94,7 +109,7 @@ def check_unfollow():
     i = 0
     while i < len(following['following']):
         if datecmp(following['following'][i]['date']):
-            print(following['following'][i]['url'] + "is followed")
+            print(following['following'][i]['url'] + " is followed")
             del following['following'][i]
             i -= 1
         i += 1
@@ -112,10 +127,11 @@ except:
     print("Hashtag file not found! Put your hashtags line by line in " + hashtag_file)
     exit(1)
 
-print(hashtags)
-
 # Open Firefox
 
+# options = Options()
+# options.headless = True
+# driver = webdriver.Firefox(options=options)
 driver = webdriver.Firefox()
 driver.get("https://www.instagram.com/accounts/login/?hl=fr&source=auth_switcher")
 time.sleep(2)
@@ -140,6 +156,7 @@ check_unfollow()
 search_bar = driver.find_element_by_xpath('/html/body/span/section/nav/div[2]/div/div/div[2]/input')
 
 search_bar.send_keys("#" + hashtags[0])
+
 time.sleep(2)
 search_bar.send_keys(Keys.ENTER)
 search_bar.send_keys(Keys.ENTER)
@@ -163,8 +180,6 @@ while len(potential_targets) < number_followers:
     time.sleep(3)
     next_button.click()
 
-print(potential_targets)
-
 driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
 
 to_follow = list()
@@ -180,14 +195,14 @@ for x in potential_targets:
     time.sleep(1)
     print(x)
     if (abo < max_abos):
-        print("Je m'abonne")
+        print("I follow, that account has just " + str(abo) + " followers", colored("<3", 'red'))
         to_follow.append(x)
         following['following'].append({
                 'date': today_str,
                 'url': x
             })
     else:
-        print("Je m'abonne pas")
+        print("I don't follow, that account has " + str(abo) + " followers!")
 
 with open('magic_book.json', 'w') as outfile:
     json.dump(following, outfile)
@@ -197,3 +212,5 @@ for x in to_follow:
 
 with open('res/followed.json', 'w') as outfile:
     json.dump(followed, outfile)
+
+conclusion()
